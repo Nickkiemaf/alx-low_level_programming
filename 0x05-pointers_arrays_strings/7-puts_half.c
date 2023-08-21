c370a254a50e4a0793a6ad8c969927c722a821ac
Nickkiemaf <mikailajibola1424@gmail.com>
2023-08-21 11:42:01 -0700

    This is the begining of my arrays, pointers, and strings journey

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_half of a string - a function that prints hald of a string
+* followed by a new line
+* @a: array name
+* @n: is the number of elements OF the array to be printed
+* str: string
+* Return: a input
+*/
+
+void puts_half(char *str)
+{
+	int a, n, longi;
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+
+		longi++;
+		n = (longi / 2);
+
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++) 
+
+		_putchar(str[a]); 
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+C - Pointers, arrays and strings
+The beginning of learning the basics of C pointers, arrays, and strings to be able to be an effective programmer
