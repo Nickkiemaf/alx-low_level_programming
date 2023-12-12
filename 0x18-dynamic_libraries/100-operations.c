77e4c41d1e47e3b4c0182316748903dd07f9e789
Nickkiemaf <mikailajibola1424@gmail.com>
2023-12-12 08:06:50 -0800

    C functions that can be used with python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * add - funtion to add two integers
+ * @x - first int
+ * @y - second int
+ * Return: sum of two ints
+ */
+int add(int x, int y)
+{
+	return x + y;
+}
+
+/**
+ * sub - function to subtract two integers
+ */
+int sub(int x, int y)
+{
+	return x - y;
+}
+
+/**
+ * mul - funtion to multiply two ints
+ */
+int mul(int x, int y)
+{
+	return x * y;
+}
+
+/**
+ * div - function to divide two integers
+ */
+int div(int x, int y)
+{
+	return x / y;
+}
+
+/**
+ * mod - funtion to return the remainder of two integers
+ */
+int moc(int x, int y)
+{
+	return x % y;
+}
